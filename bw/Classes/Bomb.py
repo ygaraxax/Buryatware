ee7a61810d8674aa8c855493c9c37e43510b6bcf
4eloveksc <58350394+FaHaT-TM@users.noreply.github.com>
2024-12-14 23:01:54 +0700

    Update Bomb.py

--- bw/Classes/Bomb.py
+++ bw/Classes/Bomb.py
@@ -9,73 +9,78 @@ from SDK.GameSDK import *
 
 
 class csBomb():
-    BombPlantedTime = 0
+    # Track bomb plant and defuse timestamps
+    BombPlantedTime = 0  
     BombDefusedTime = 0
 
-    
+    @staticmethod
     def getC4BaseClass():
+        """Get base pointer to planted C4 entity"""
         PlantedC4Class = Memory.Read.longlong(Game.Client + dwPlantedC4)
         return Memory.Read.longlong(PlantedC4Class)
-    
 
+    @staticmethod 
     def getPositionWTS():
-        C34Position = Vector3()
-
+        """Get bomb position in world-to-screen coordinates"""
+        C4Position = Vector3()
         C4Node = Memory.Read.longlong(csBomb.getC4BaseClass() + m_pGameSceneNode)
 
-        C34Position.x = Memory.Read.float(C4Node + m_vecAbsOrigin)
-        C34Position.y = Memory.Read.float(C4Node + m_vecAbsOrigin + 0x4)
-        C34Position.z = Memory.Read.float(C4Node + m_vecAbsOrigin + 0x8)
+        # Read XYZ coordinates
+        C4Position.x = Memory.Read.float(C4Node + m_vecAbsOrigin)
+        C4Position.y = Memory.Read.float(C4Node + m_vecAbsOrigin + 0x4)
+        C4Position.z = Memory.Read.float(C4Node + m_vecAbsOrigin + 0x8)
 
         Matrix = SDK.getViewMatrix()
-        return SDK.WorldToScreen(C34Position, Matrix)
-    
+        return SDK.WorldToScreen(C4Position, Matrix)
 
+    @staticmethod
     def getSite():
+        """Get bomb site identifier (A or B)"""
         Site = Memory.Read.int(csBomb.getC4BaseClass() + m_nBombSite)
-        return "А" if (Site != 1) else "B"
-    
+        return "А" if Site != 1 else "B"
 
+    @staticmethod
     def isPlanted():
+        """Check if bomb is planted and track plant time"""
         BombIsPlanted = Memory.Read.bool(Game.Client + dwPlantedC4 - 0x8)
 
-        if (BombIsPlanted):
-            if (csBomb.BombPlantedTime == 0):
-                csBomb.BombPlantedTime = time.time() 
-
-        else:
+        if BombIsPlanted and csBomb.BombPlantedTime == 0:
+            csBomb.BombPlantedTime = time.time()
+        elif not BombIsPlanted:
             csBomb.BombPlantedTime = 0
 
         return BombIsPlanted
-    
 
+    @staticmethod
     def isBeingDefused():
+        """Check if bomb is being defused and track defuse time"""
         BombIsDefused = Memory.Read.bool(csBomb.getC4BaseClass() + m_bBeingDefused)
 
-        if (BombIsDefused):
-            if (csBomb.BombDefusedTime == 0):
-                csBomb.BombDefusedTime = time.time() 
-
-        else:
+        if BombIsDefused and csBomb.BombDefusedTime == 0:
+            csBomb.BombDefusedTime = time.time()
+        elif not BombIsDefused:
             csBomb.BombDefusedTime = 0
 
         return BombIsDefused
-    
 
+    @staticmethod
     def getDefuseLength():
+        """Get total defuse time required"""
         return Memory.Read.float(csBomb.getC4BaseClass() + m_flDefuseLength)
-    
 
+    @staticmethod
     def getTimerLength():
+        """Get total explosion timer length"""
         return Memory.Read.float(csBomb.getC4BaseClass() + m_flTimerLength)
-    
 
+    @staticmethod
     def getBombTime():
+        """Get remaining time until explosion"""
         BombTime = csBomb.getTimerLength() - (time.time() - csBomb.BombPlantedTime)
-        return BombTime if (BombTime >= 0) else 0
-    
+        return max(BombTime, 0)
 
+    @staticmethod
     def getDefuseTime():
+        """Get remaining defuse time if being defused"""
         DefuseTime = csBomb.getDefuseLength() - (time.time() - csBomb.BombDefusedTime)
         return DefuseTime if (csBomb.isBeingDefused() and DefuseTime >= 0) else 0
-
