7ce71f2058a16a7b3b0d30c360e03e4c09ef7461
4eloveksc <58350394+FaHaT-TM@users.noreply.github.com>
2024-12-14 22:55:32 +0700

    Update Burware.py

--- bw/Burware.py
+++ bw/Burware.py
@@ -1,93 +1,76 @@
+# Import required modules
 from os import system
-
 system("pip install win32api")
 
 import SDK.GameLoop as GameLoop
-
-
-
 from Tool.Process import *
 import UI.MainUI as GUI
-import UI.AntiAimAsistUI as AntiAimAssistWindow
-import threading, signal, sys, os, webbrowser
 import UI.AntiAimAsistUI as AntiAimWindow
+import threading
+import signal
+import sys
+import os
+import webbrowser
 
+# Import cheat functions
 import Function.Visual as Visual
-import Function.AimBot as AimBot
+import Function.AimBot as AimBot 
 import Function.TriggerBot as TriggerBot
 import Function.AutoPistol as AutoPistol
 import Function.BunnyHop as BunnyHop
 import Function.AntiAim as AntiAim
 
-
+# Initialize game connection
 Game.Connect("cs2.exe", "Counter-Strike 2")
-GameLoop.GameLoop.Start()   
-
+GameLoop.GameLoop.Start()
 
 if __name__ == "__main__":
-   app = GUI.QtWidgets.QApplication(sys.argv)
-   ui = GUI.Widget()
-   ui.show()
+    # Initialize UI
+    app = GUI.QtWidgets.QApplication(sys.argv)
+    ui = GUI.Widget()
+    ui.show()
 
-   def StartVisual():
-      Visual.VisualStatus = ui.visualCheckBox.checkState()
+    # Helper functions to start cheat features
+    def start_feature(feature, checkbox):
+        """Generic function to start a cheat feature in a new thread"""
+        feature.Status = checkbox.checkState()
+        if feature.Status:
+            threading.Thread(target=feature.Function).start()
 
-      if (Visual.VisualStatus):
-         threading.Thread(target = Visual.Function).start()
+    def StartVisual():
+        start_feature(Visual, ui.visualCheckBox)
 
+    def StartAimBot():
+        start_feature(AimBot, ui.aimAsistCheckBox)
 
-   def StartAimBot():
-      AimBot.AimBotStatus = ui.aimAsistCheckBox.checkState()
+    def StartTriggerBot():
+        start_feature(TriggerBot, ui.triggerBotCheckBox)
 
-      if (AimBot.AimBotStatus):
-         threading.Thread(target = AimBot.Function).start()
+    def StartAutoPistol():
+        start_feature(AutoPistol, ui.autoPistolCheckBox)
 
+    def StartBunnyHop():
+        start_feature(BunnyHop, ui.bunnyHopCheckBox)
 
-   def StartTriggerBot():
-      TriggerBot.TriggerBotStatus = ui.triggerBotCheckBox.checkState()
+    def StartAntiAim():
+        start_feature(AntiAim, ui.antiAimCheckBox)
 
-      if (TriggerBot.TriggerBotStatus):
-         threading.Thread(target = TriggerBot.Function).start()
+    # Connect UI buttons to settings windows
+    ui.visualSettingButton.clicked.connect(ui.ShowVisualWindow)
+    ui.aimAsistSettingButton.clicked.connect(ui.ShowAimBotWindow)
+    ui.triggerBotSettingButton.clicked.connect(ui.ShowTriggerBotWindow)
+    ui.autoPistolSettingButton.clicked.connect(ui.ShowAutoPistolWindow)
 
+    # Connect checkboxes to feature start functions
+    ui.visualCheckBox.stateChanged.connect(StartVisual)
+    ui.aimAsistCheckBox.stateChanged.connect(StartAimBot)
+    ui.triggerBotCheckBox.stateChanged.connect(StartTriggerBot)
+    ui.autoPistolCheckBox.stateChanged.connect(StartAutoPistol)
+    ui.bunnyHopCheckBox.stateChanged.connect(StartBunnyHop)
+    ui.antiAimCheckBox.stateChanged.connect(StartAntiAim)
 
-   def StartAutoPistol():
-      AutoPistol.AutoPistolStatus = ui.autoPistolCheckBox.checkState()
+    # Connect utility buttons
+    ui.closeButton.clicked.connect(lambda: os.kill(os.getpid(), signal.SIGTERM))
+    ui.questionButton.clicked.connect(lambda: webbrowser.open("https://t.me/burware/", new=2))
 
-      if (AutoPistol.AutoPistolStatus):
-         threading.Thread(target = AutoPistol.Function).start()
-
-
-   def StartBunnyHop():
-      BunnyHop.BunnyHopStatus = ui.bunnyHopCheckBox.checkState()
-
-      if (BunnyHop.BunnyHopStatus):
-         threading.Thread(target = BunnyHop.Function).start()
-
-
-   def StartAntiAim():
-      AntiAim.AntiAimStatus = ui.antiAimCheckBox.checkState()
-
-      if (AntiAim.AntiAimStatus):
-         threading.Thread(target = AntiAim.Function).start()     
-         
-         
-         
-
-   ui.visualSettingButton.clicked.connect(ui.ShowVisualWindow)
-   ui.aimAsistSettingButton.clicked.connect(ui.ShowAimBotWindow)
-   ui.triggerBotSettingButton.clicked.connect(ui.ShowTriggerBotWindow)
-   ui.autoPistolSettingButton.clicked.connect(ui.ShowAutoPistolWindow)
-  
-
-   ui.visualCheckBox.stateChanged.connect(StartVisual)
-   ui.aimAsistCheckBox.stateChanged.connect(StartAimBot)
-   ui.triggerBotCheckBox.stateChanged.connect(StartTriggerBot)
-   ui.autoPistolCheckBox.stateChanged.connect(StartAutoPistol)
-   ui.bunnyHopCheckBox.stateChanged.connect(StartBunnyHop)
-   ui.antiAimCheckBox.stateChanged.connect(StartAntiAim)
-   
-   ui.closeButton.clicked.connect(lambda: os.kill(os.getpid(), signal.SIGTERM))
-   ui.questionButton.clicked.connect(lambda: webbrowser.open("https://t.me/burware/", new = 2))
-   sys.exit(app.exec_())
-
-    
+    sys.exit(app.exec_())
